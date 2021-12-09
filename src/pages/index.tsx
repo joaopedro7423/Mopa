2b7c9bccb55d5b7c5df492b34fd8892e764366fb
joaopedro7423 <joaopedro7423@hotmail.com>
2021-12-09 09:39:00 -0300

    chore: config chakra to project

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import { Heading, Text } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h3 >Helo Word</h3>
+
+      <Text color="mopa.primary">sanfsalfnlasnfl</Text>
+
+      <Heading
+      color="mopa.600"
+      _hover={{color: "red"}}
+      transition="ease 500ms"
+      >gnjasgjnnasjgnn</Heading>
+    </>
+  );
+};
+
+export default Home;
