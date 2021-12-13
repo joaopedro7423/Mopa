719225c9f8417fb40101b1f9c4f70ee1d6b7d0a4
joaopedro7423 <joaopedro7423@hotmail.com>
2021-12-13 16:24:20 -0300

    feat: create perfilImage

---
+++ src/components/ImagePerfil.tsx
@@ -0,0 +1,25 @@
+import { Box, Center, Image, Text } from "@chakra-ui/react";
+
+type ImageProps = {
+  img: string;
+  text: string;
+};
+
+export function ImagePerfil({ img, text }: ImageProps) {
+  return (
+    <>
+      <Center flexDir="column">
+        <Image
+          maxW="150px"
+          maxH="150px"
+          borderRadius="full"
+          src={img}
+          alt="Users"
+        />
+        <Text pt={5} textAlign="center" justifyContent="center">
+          {text}
+        </Text>
+      </Center>
+    </>
+  );
+}
