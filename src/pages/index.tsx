6ed77f000e94f7e17d5e08357ef2b682da002bab
joaopedro7423 <joaopedro7423@hotmail.com>
2021-12-14 16:47:53 -0300

    refactor: add initialParalax to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import { Center, Container, Flex, HStack, Text } from "@chakra-ui/react";
+import { Box, Center, Container, Flex, HStack, Text } from "@chakra-ui/react";
 import { ImagePerfil } from "components/shared/ImagePerfil";
 import { SpecialMembers } from "components/indexSections/SpecialMembers";
 import { MainLayout } from "layout/MainLayout";
 import type { NextPage } from "next";
 import { NextSeo } from "next-seo";
+import { InitialParalax } from "components/indexSections/InitialParalax";
 
 const Home: NextPage = () => {
   return (
@@ -13,9 +14,9 @@ const Home: NextPage = () => {
         description="Nunca conhecemos a derrota."
       ></NextSeo>
       <MainLayout>
+        <InitialParalax id="inicio" />
 
-        <SpecialMembers id="MembrosEspeciais"/>
-
+        <SpecialMembers id="MembrosEspeciais" />
       </MainLayout>
     </>
   );
