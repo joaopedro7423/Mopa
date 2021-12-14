6b2e240d76f2738c95d26d044353cf91b51c2afa
joaopedro7423 <joaopedro7423@hotmail.com>
2021-12-14 10:13:30 -0300

    refactor: use Special Members component  in to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,23 @@
+import { Center, Container, Flex, HStack, Text } from "@chakra-ui/react";
+import { ImagePerfil } from "components/shared/ImagePerfil";
+import { SpecialMembers } from "components/indexSections/SpecialMembers";
 import { MainLayout } from "layout/MainLayout";
 import type { NextPage } from "next";
 import { NextSeo } from "next-seo";
 
 const Home: NextPage = () => {
   return (
-    <MainLayout>
+    <>
       <NextSeo
         title="MOPA TEAM | OS VERDADEIROS ESTÃO AQUI!"
         description="Nunca conhecemos a derrota."
       ></NextSeo>
-      Um teste de Escrita
-    </MainLayout>
+      <MainLayout>
+
+        <SpecialMembers />
+
+      </MainLayout>
+    </>
   );
 };
 
