c1b11aa64fbdd6dda6be11575d1066d7d044758c
joaopedro7423 <joaopedro7423@hotmail.com>
2021-12-14 10:58:14 -0300

    feat: add id to Specialmembers component to separate Section

--- src/components/indexSections/SpecialMembers.tsx
+++ src/components/indexSections/SpecialMembers.tsx
@@ -1,28 +1,34 @@
-import { Center, HStack } from "@chakra-ui/react";
+import { Box, Center, HStack } from "@chakra-ui/react";
 import { ImagePerfil } from "components/shared/ImagePerfil";
 import { Paralax } from "components/shared/Paralax";
 
-export function SpecialMembers() {
+interface SpecialMembersProps {
+  id?: string;
+}
+
+export function SpecialMembers({ id }: SpecialMembersProps) {
   return (
-    <Paralax image="/images/logo_header.jpg" p={5}>
-      <Center>
-        <HStack spacing={8} display={{ base: "block", md: "flex" }}>
-          <ImagePerfil
-            img="/images/logo_header.jpg"
-            text="Um texto muito divertido eeeeeee"
-          />
+    <Box id={id}>
+      <Paralax image="/images/logo_header.jpg" p={5}>
+        <Center>
+          <HStack spacing={8} display={{ base: "block", md: "flex" }}>
+            <ImagePerfil
+              img="/images/logo_header.jpg"
+              text="Um texto muito divertido eeeeeee"
+            />
 
-          <ImagePerfil
-            img="/images/logo_header.jpg"
-            text="Um texto muito divertido eeeeeee"
-          />
+            <ImagePerfil
+              img="/images/logo_header.jpg"
+              text="Um texto muito divertido eeeeeee"
+            />
 
-          <ImagePerfil
-            img="/images/logo_header.jpg"
-            text="Um texto muito divertido eeeeeee"
-          />
-        </HStack>
-      </Center>
-    </Paralax>
+            <ImagePerfil
+              img="/images/logo_header.jpg"
+              text="Um texto muito divertido eeeeeee"
+            />
+          </HStack>
+        </Center>
+      </Paralax>
+    </Box>
   );
 }
