812314ed046d33a80d72e3476b5d56b6560a9b6b
joaopedro7423 <joaopedro7423@hotmail.com>
2021-12-15 16:24:56 -0300

    refactor: define box w h in Image Perfil component shered

--- src/components/shared/ImagePerfil.tsx
+++ src/components/shared/ImagePerfil.tsx
@@ -3,20 +3,14 @@ import { Box, Center, color, Image, Text } from "@chakra-ui/react";
 type ImageProps = {
   img: string;
   text: string;
-  color?: string ;
+  color?: string;
 };
 
-export function ImagePerfil({ img, text, color = 'white' }: ImageProps) {
+export function ImagePerfil({ img, text, color = "white" }: ImageProps) {
   return (
     <>
       <Center flexDir="column">
-        <Image
-          maxW="150px"
-          maxH="150px"
-          borderRadius="full"
-          src={img}
-          alt="Users"
-        />
+        <Image boxSize="200px" borderRadius="full" src={img} alt="Images" />
         <Text color={color} pt={5} textAlign="center" justifyContent="center">
           {text}
         </Text>
