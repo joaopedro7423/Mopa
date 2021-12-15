866852f7d40b4c4e9c352f12b09d4af1aa4c2f7e
joaopedro7423 <joaopedro7423@hotmail.com>
2021-12-15 16:39:44 -0300

    refactor: chenge stack for flex box component section SpecialMembers

--- src/components/indexSections/SpecialMembers.tsx
+++ src/components/indexSections/SpecialMembers.tsx
@@ -1,6 +1,5 @@
-import { Box, Center, HStack, Stack } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { ImagePerfil } from "components/shared/ImagePerfil";
-import { Paralax } from "components/shared/Paralax";
 
 interface SpecialMembersProps {
   id?: string;
@@ -9,31 +8,30 @@ interface SpecialMembersProps {
 export function SpecialMembers({ id }: SpecialMembersProps) {
   return (
     <Box id={id} p={5} backgroundColor="black">
-      <Center>
-        <Stack
-          spacing="60px"
-          display={{ base: "block", md: "flex" }}
-          direction={{ base: "column", md: "row" }}
-        >
-          <ImagePerfil
-            img="/images/logo_header.jpg"
-            text="Um texto muito divertido eeeeeee"
-            color="white"
-          />
+      <Flex
+        justifyContent="space-between"
+        maxW="70%"
+        margin="auto"
+        display={{ base: "block", md: "flex" }}
+      >
+        <ImagePerfil
+          img="/images/logo_header.jpg"
+          text="Um texto muito divertido eeeeeee"
+          color="white"
+        />
 
-          <ImagePerfil
-            img="/images/logo_header.jpg"
-            text="Um texto muito divertido eeeeeee"
-            color="pink"
-          />
+        <ImagePerfil
+          img="/images/logo_header.jpg"
+          text="Um texto muito divertido eeeeeee"
+          color="pink"
+        />
 
-          <ImagePerfil
-            img="/images/logo_header.jpg"
-            text="Um texto muito divertido eeeeeee"
-            color="red"
-          />
-        </Stack>
-      </Center>
+        <ImagePerfil
+          img="/images/logo_header.jpg"
+          text="Um texto muito divertido eeeeeee"
+          color="red"
+        />
+      </Flex>
     </Box>
   );
 }
