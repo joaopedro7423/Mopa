4cccdef9766faa539124555dd52826765123bf91
joaopedro7423 <joaopedro7423@hotmail.com>
2021-12-16 16:26:00 -0300

    feat: create a product list and set params

---
+++ src/navigation/productsList.ts
@@ -0,0 +1,16 @@
+import { ProductList } from "types/product";
+
+export const productList: ProductList[] = [
+  {
+    title: "Copo Stanley",
+    description: "O copo mais foda de todo o praneta",
+    image: "/images/products/stanley2.jpg",
+    urlStripe: "https://www.youtube.com",
+  },
+  {
+    title: "Pochete MOPA",
+    description: "A pochete mais foda de todo o praneta, arrasa gatinhas",
+    image: "/images/products/pochete.jpg",
+    urlStripe: "https://www.youtube.com",
+  }
+];
---
+++ src/types/product.d.ts
@@ -0,0 +1,6 @@
+export interface ProductList{
+    title:string;
+    description:string;
+    urlStripe:string;
+    image:string;
+}
