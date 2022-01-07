0d4fb2d01235d8c45df748f85b2ba1e597155fd1
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-01-06 21:23:40 -0400

    refactor: add paralax section and chenge text color

--- src/components/indexSections/GamePlayed.tsx
+++ src/components/indexSections/GamePlayed.tsx
@@ -1,5 +1,6 @@
 import { Box, Center, Flex, Stack } from "@chakra-ui/react";
 import { ImagePerfil } from "components/shared/ImagePerfil";
+import { Paralax } from "components/shared/Paralax";
 
 interface GamePlayedProps {
   id?: string;
@@ -7,19 +8,19 @@ interface GamePlayedProps {
 
 export function GamePlayed({ id }: GamePlayedProps) {
   return (
-    <Box id={id} p={5} backgroundColor="black">
+    <Paralax image="/images/MOPA_3.jpg" id={id} p={5} backgroundColor="black">
       <Flex
         justifyContent="space-between"
         maxW="70%"
         margin="auto"
         display={{ base: "block", md: "flex" }}
       >
-        <ImagePerfil img="/images/csgo.jpg" text="Cs Go" color="white" />
+        <ImagePerfil img="/images/csgo.jpg" text="Cs Go" color="Red" />
 
-        <ImagePerfil img="/images/lol.jpg" text="LOL" />
+        <ImagePerfil img="/images/lol.jpg" text="LOL" color="Red" />
 
-        <ImagePerfil img="/images/cod.jpg" text="Cod Warzone" />
+        <ImagePerfil img="/images/cod.jpg" text="Cod Warzone" color="Red" />
       </Flex>
-    </Box>
+    </Paralax>
   );
 }
