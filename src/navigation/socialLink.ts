033a24376340db03ae3136842850d6de98a321dc
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-01-31 11:46:31 -0400

    feat: add nes const social links

---
+++ src/navigation/socialLink.ts
@@ -0,0 +1,6 @@
+export const discordLink = "https://discord.gg/uMzXaqmZZP";
+export const sopotfyLink = "";
+export const youtubeLink = "";
+export const instagranLink = "";
+export const twiterLink = "";
+export const Link = "";
