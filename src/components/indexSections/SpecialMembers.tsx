88f6ec44e8f270a84f897eb5c71f8daa7d35afd0
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-01-31 14:32:57 -0400

    refactor: add members list into secction

--- src/components/indexSections/SpecialMembers.tsx
+++ src/components/indexSections/SpecialMembers.tsx
@@ -1,5 +1,6 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Wrap, WrapItem } from "@chakra-ui/react";
 import { ImagePerfil } from "components/shared/ImagePerfil";
+import { memberList } from "navigation/memberList";
 
 interface SpecialMembersProps {
   id?: string;
@@ -8,33 +9,19 @@ interface SpecialMembersProps {
 export function SpecialMembers({ id }: SpecialMembersProps) {
   return (
     <Box id={id} p={5} backgroundColor="black">
-      <Flex
-        justifyContent="space-between"
-        maxW="70%"
-        margin="auto"
-        display={{ base: "block", md: "flex" }}
-      >
-        <ImagePerfil
-          link="null"
-          img="/images/logo_header.jpg"
-          text="Um texto muito divertido eeeeeee"
-          color="white"
-        />
-
-        <ImagePerfil
-          link="null"
-          img="/images/logo_header.jpg"
-          text="Um texto muito divertido eeeeeee"
-          color="pink"
-        />
-
-        <ImagePerfil
-          link="null"
-          img="/images/logo_header.jpg"
-          text="Um texto muito divertido eeeeeee"
-          color="red"
-        />
-      </Flex>
+      <Wrap justify="center" maxW="80%" margin="auto">
+        {memberList.map((member, index) => (
+          <WrapItem key={index}>
+            <ImagePerfil
+              link="null"
+              img={member.image}
+              description={member.description}
+              text={member.text}
+              color="white"
+            />
+          </WrapItem>
+        ))}
+      </Wrap>
     </Box>
   );
 }
