3f2199acde1b2e5cdc232b8f429fadc7bd6bd8df
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-01-31 14:32:02 -0400

    feat: create a member list

---
+++ src/navigation/memberList.ts
@@ -0,0 +1,37 @@
+import { MemberList } from "types/member";
+
+const baseImage = "/images/members/";
+
+export const memberList: MemberList[] = [
+  {
+    text: "Sidao",
+    description: "O PADRINHO MASTER",
+    image: `${baseImage}sid.jpg`,
+  },
+  { text: "Jão", description: "O DEV", image: `${baseImage}joao.jpg` },
+  {
+    text: "Pedro",
+    description: " O ARQUITETO",
+    image: `${baseImage}pedro.jpg`,
+  },
+  { text: "Tiosso", description: "O DESIGN", image: `${baseImage}tiosso.jpg` },
+  {
+    text: "Felipe",
+    description: "O CURITIBANO",
+    image: `${baseImage}felipe.jpg`,
+  },
+  { text: "Lucas", description: " O COWBOY", image: `${baseImage}lucas.jpg` },
+  { text: "João", description: "O MUSQUITO", image: `${baseImage}muska.jpg` },
+  { text: "Raul", description: "O RAUL", image: `${baseImage}raul.jpg` },
+  { text: "Freire", description: "O FÍSICO", image: `${baseImage}freire.jpg` },
+  {
+    text: "Maria",
+    description: "A MÃE DO ROLE",
+    image: `${baseImage}maria.jpg`,
+  },
+  {
+    text: "Vitor",
+    description: "O PAI DO ROLE",
+    image: `${baseImage}vitor.jpg`,
+  },
+];
