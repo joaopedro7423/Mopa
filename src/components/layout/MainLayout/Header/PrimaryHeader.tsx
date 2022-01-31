7a857b08d5f49e6f3be1f14a67654992f20239fa
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-01-31 09:42:15 -0400

    feat: add box shadown and opacit to box

--- src/components/layout/MainLayout/Header/PrimaryHeader.tsx
+++ src/components/layout/MainLayout/Header/PrimaryHeader.tsx
@@ -36,6 +36,8 @@ export function PrimaryHeader() {
 
   return (
     <Box
+      backgroundColor="rgba(0, 0, 0, 0.5)"
+      boxShadow={`0px 0px 10px rgba(0, 0, 0, 0.1)`}
       position="sticky"
       left="0"
       top="0"
