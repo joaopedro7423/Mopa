f2da9816734b17c70a3e6acdb1a9760e526b0ff5
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-01-31 12:45:30 -0400

    feat: add links to social link

--- src/navigation/socialLink.ts
+++ src/navigation/socialLink.ts
@@ -1,6 +1,8 @@
 export const discordLink = "https://discord.gg/uMzXaqmZZP";
-export const sopotfyLink = "";
-export const youtubeLink = "";
-export const instagranLink = "";
+export const sopotfyLink =
+  "https://open.spotify.com/playlist/76zlJVr7Fzc9pYqMA1J8iV?si=5cd1122ecd634d8d";
+export const youtubeLink =
+  "https://www.youtube.com/playlist?list=PLBoz5W2xPXRD1YO5cE2mL32Az4Vj6-JAC";
+export const instagranLink = "https://www.instagram.com/mopateamf/";
 export const twiterLink = "";
-export const Link = "";
+//export const Link = "";
