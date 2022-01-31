7485eec0a8d7db33194489efe892b5c3d095ddbc
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-01-31 13:18:30 -0400

    refactor: chenge const to list navigation

--- src/navigation/socialLink.ts
+++ src/navigation/socialLink.ts
@@ -1,8 +1,19 @@
-export const discordLink = "https://discord.gg/uMzXaqmZZP";
-export const sopotfyLink =
-  "https://open.spotify.com/playlist/76zlJVr7Fzc9pYqMA1J8iV?si=5cd1122ecd634d8d";
-export const youtubeLink =
-  "https://www.youtube.com/playlist?list=PLBoz5W2xPXRD1YO5cE2mL32Az4Vj6-JAC";
-export const instagranLink = "https://www.instagram.com/mopateamf/";
-export const twiterLink = "";
-//export const Link = "";
+import { SocialList } from "types/SocialList";
+
+export const socialList: SocialList[] = [
+  {
+    title: "Spotify",
+    image: "/images/spotfy.png",
+    url: "https://open.spotify.com/playlist/76zlJVr7Fzc9pYqMA1J8iV?si=5cd1122ecd634d8d",
+  },
+  {
+    title: "Instagram",
+    image: "/images/insta.png",
+    url: "https://www.instagram.com/mopateamf/",
+  },
+  {
+    title: "Youtube",
+    image: "/images/ym.png",
+    url: "https://www.youtube.com/playlist?list=PLBoz5W2xPXRD1YO5cE2mL32Az4Vj6-JAC",
+  },
+];
