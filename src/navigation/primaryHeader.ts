5258eb6281f8f19a1917eb046b17c9913894e8d0
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-01-31 12:43:14 -0400

    feat: add header link to social media

--- src/navigation/primaryHeader.ts
+++ src/navigation/primaryHeader.ts
@@ -1,13 +1,11 @@
 import { DropdownOptions } from "types/dropdown";
 
 export const primaryHeaderNavigation: DropdownOptions[] = [
- 
- 
   { name: "Inicio", url: "#inicio" },
   { name: "Quem Somos", url: "#QuemNosSomos" },
   { name: "Jogos", url: "#JogosJogados" },
   { name: "Nosso Discord", url: "#NossoDiscord" },
+  { name: "Social", url: "#SocialMedia" },
   { name: "Produtos", url: "#Produtos" },
   { name: "Membros Especiais", url: "#MembrosEspeciais" },
- 
 ];
