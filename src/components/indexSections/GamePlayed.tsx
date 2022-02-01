99b2193df59204db1e0a369c6672f1dffa90778a
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-02-01 11:38:07 -0400

    refactor: use warp in game played and chenge image paralax end text in image perfil

--- src/components/indexSections/GamePlayed.tsx
+++ src/components/indexSections/GamePlayed.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Flex, Stack } from "@chakra-ui/react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { ImagePerfil } from "components/shared/ImagePerfil";
 import { Paralax } from "components/shared/Paralax";
 
@@ -8,41 +8,50 @@ interface GamePlayedProps {
 
 export function GamePlayed({ id }: GamePlayedProps) {
   return (
-    <Paralax image="/images/MOPA_3.jpg" id={id} p={5} backgroundColor="black">
-      <Flex
-        justifyContent="space-between"
-        maxW="70%"
-        margin="auto"
-        display={{ base: "block", md: "flex" }}
-      >
-        <ImagePerfil
-          link="null"
-          img="/images/csgo.jpg"
-          text="Cs Go"
-          color="black"
-        />
+    <Paralax
+      image="/images/fundo_branco.png"
+      id={id}
+      p={5}
+      backgroundColor="black"
+    >
+      <Wrap justify="center" maxW="80%" margin="auto">
+        <WrapItem>
+          <ImagePerfil
+            link="null"
+            img="/images/csgo.jpg"
+            text="CS GO"
+            color="black"
+          />
+        </WrapItem>
 
-        <ImagePerfil
-          link="null"
-          img="/images/lol.jpg"
-          text="LOL"
-          color="black"
-        />
+        <WrapItem>
+          <ImagePerfil
+            link="null"
+            img="/images/lol.jpg"
+            text="LOL"
+            color="black"
+          />
+        </WrapItem>
 
-        <ImagePerfil
-          link="null"
-          img="/images/cod.jpg"
-          text="Cod Warzone"
-          color="black"
-        />
-
-        <ImagePerfil
-          link="null"
-          img="/images/apex.jpg"
-          text="APEX"
-          color="black"
-        />
-      </Flex>
+        <WrapItem>
+          {" "}
+          <ImagePerfil
+            link="null"
+            img="/images/cod.jpg"
+            text="COD WARZONE"
+            color="black"
+          />
+        </WrapItem>
+        <WrapItem>
+          {" "}
+          <ImagePerfil
+            link="null"
+            img="/images/apex.jpg"
+            text="APEX"
+            color="black"
+          />
+        </WrapItem>
+      </Wrap>
     </Paralax>
   );
 }
