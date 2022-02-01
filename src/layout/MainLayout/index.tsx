6ff59962b6e242f21b1213c93998fd67418b57d2
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-02-01 11:39:20 -0400

    refactor: remove scroll personality

--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -23,24 +23,12 @@ export function MainLayout({ children }: MainLayoutProps) {
   useEffect(() => {
     window.addEventListener("scroll", toogleTop);
   }, []);
+
   return (
     <Flex direction="column">
       {isTop ? <Header /> : null}
 
-      <Content
-        sx={{
-          "&::-webkit-scrollbar": {
-            width: "16px",
-            borderRadius: "8px",
-            backgroundColor: `rgba(0, 0, 0, 0.05)`,
-          },
-          "&::-webkit-scrollbar-thumb": {
-            backgroundColor: `rgba(0, 0, 0, 0.05)`,
-          },
-        }}
-      >
-        {children}
-      </Content>
+      <Content>{children}</Content>
 
       <Footer />
     </Flex>
