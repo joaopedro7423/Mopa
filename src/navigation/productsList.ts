5d215c3c146b40871942905b2e525ae1ad60bb5d
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-01-31 20:19:22 -0400

    refactor: chenge link end desciption

--- src/navigation/productsList.ts
+++ src/navigation/productsList.ts
@@ -3,14 +3,14 @@ import { ProductList } from "types/product";
 export const productList: ProductList[] = [
   {
     title: "Copo Stanley",
-    description: "O copo mais foda de todo o praneta",
+    description: "O copo mais DAORA de todo o planeta",
     image: "/images/products/stanley2.jpg",
-    urlStripe: "https://www.youtube.com",
+    urlStripe: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
   },
   {
     title: "Pochete MOPA",
-    description: "A pochete mais foda de todo o praneta, arrasa gatinhas",
+    description: "A pochete mais DAORA de todo o planeta, arrasa gatinhas",
     image: "/images/products/pochete.jpg",
-    urlStripe: "https://www.youtube.com",
-  }
+    urlStripe: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  },
 ];
