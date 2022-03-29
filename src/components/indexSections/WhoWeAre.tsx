cb1e232ee59387ab4a67a07038823a026f751507
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-03-28 23:11:42 -0400

    refactor: add a paralax and import Neon Card

--- src/components/indexSections/WhoWeAre.tsx
+++ src/components/indexSections/WhoWeAre.tsx
@@ -1,5 +1,7 @@
 import { Center, Divider, Flex, Image, Text } from "@chakra-ui/react";
 import { ImageWithTextMidle } from "components/shared/ImageWithTextMidle";
+import NeonCard from "components/shared/NeonCard";
+import { Paralax } from "components/shared/Paralax";
 
 interface WhoWeAreProps {
   id?: string;
@@ -7,13 +9,20 @@ interface WhoWeAreProps {
 
 export function WhoWeAre({ id }: WhoWeAreProps) {
   return (
-    <Center id={id} minh="50vh" bgColor="black">
+    <Paralax
+      py={20}
+      bgSize="contain"
+      id={id}
+      image="/images/background/carbon.jpg"
+    >
+      <NeonCard></NeonCard>
+      {/*       
       <ImageWithTextMidle
         alignLeft
         text="Somos mais que um time. Somos MOPA TEAM."
         imagePath="/images/caveiraMopa.png"
         url="https://www.youtube.com/watch?v=pfBMfgLYum4"
-      />
-    </Center>
+      /> */}
+    </Paralax>
   );
 }
