9b0e79b0cf01a0e6627bfc01d665eb1b7d12dd97
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-03-28 23:09:27 -0400

    feat: create css to use worklet box and import to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/themes";
 import "styles/themes/fonts.css";
+import "styles/themes/polygonBox.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
