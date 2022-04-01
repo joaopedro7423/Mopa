f1f9efbd3c8b99f45b764fa6234646e18f4e8f86
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-03-31 22:57:59 -0400

    refactor: new layout to component Products

--- src/components/indexSections/Products.tsx
+++ src/components/indexSections/Products.tsx
@@ -1,5 +1,6 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text, chakra, VStack, Stack, Container } from "@chakra-ui/react";
 import { CarouselChakra } from "components/shared/Carrousel";
+import { Link } from "components/shared/Link";
 
 interface ProductsProps {
   id: string;
@@ -7,10 +8,28 @@ interface ProductsProps {
 
 export function Products({ id }: ProductsProps) {
   return (
-    <Box pt={12} id={id} minh="50vh" bgColor="black" p={5}>
-      <Box w="80%" margin="auto" p={12}>
-        <CarouselChakra />
+    <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" isExternal>
+      <Box id={id} py={20} bgImage={"/images/background/branco.jpg"}>
+        <Box h="100%" maxW={{ bg: "720px", lg: "1430px" }} w={"80%"} m={"auto"}>
+          <Box ml={10} justifyContent={"initial"} maxW={"360px"}>
+            <Text
+              color={"black"}
+              fontFamily={"grungie"}
+              fontSize={["100px", "100px", "100px", "120px"]}
+              lineHeight={0.6}
+            >
+              PRODUTOS
+              <chakra.span
+                lineHeight={0.5}
+                fontSize={["170px", "170px", "170px", "200px"]}
+              >
+                {" "}
+                MOPA
+              </chakra.span>
+            </Text>
+          </Box>
+        </Box>
       </Box>
-    </Box>
+    </Link>
   );
 }
