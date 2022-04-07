f263c95a74de98442460aeb27835b9a912605529
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-04-06 22:47:48 -0400

    refactor: chenge social image

--- src/navigation/socialLink.ts
+++ src/navigation/socialLink.ts
@@ -4,18 +4,18 @@ export const discordLink = "https://discord.gg/uMzXaqmZZP";
 
 export const socialList: SocialList[] = [
   {
-    title: "Spotify",
-    image: "/images/spotfy.png",
-    url: "https://open.spotify.com/playlist/76zlJVr7Fzc9pYqMA1J8iV?si=5cd1122ecd634d8d",
-  },
-  {
     title: "Instagram",
-    image: "/images/insta.png",
+    image: "/images/social/insta.png",
     url: "https://www.instagram.com/mopateamf/",
   },
   {
+    title: "Spotify",
+    image: "/images/social/spotfy.png",
+    url: "https://open.spotify.com/playlist/76zlJVr7Fzc9pYqMA1J8iV?si=5cd1122ecd634d8d",
+  },
+  {
     title: "Youtube",
-    image: "/images/ym.png",
+    image: "/images/social/ym.png",
     url: "https://www.youtube.com/playlist?list=PLBoz5W2xPXRD1YO5cE2mL32Az4Vj6-JAC",
   },
 ];
